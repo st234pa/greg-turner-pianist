0e2ccb177ce8ca2e936571bbafd8bd05071efaa8
Greg <gregturnerpianist@gmail.com>
2024-07-21 14:24:08 -0400

    fixed gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,13 +20,14 @@ export default function App({ Component, pageProps }: AppProps) {
       <NextThemesProvider forcedTheme="light">
         <GoogleReCaptchaProvider reCaptchaKey="6LcClgQqAAAAACAEkhusFmItV5nv66CPXVXGncqg">
           <Component {...pageProps} />
-          <Script>
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5MK2H9TX');`}
-          </Script>
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+          >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5MK2H9TX');`}</Script>
           <Script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-KTEJXPYXPG"
