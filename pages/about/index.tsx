2afda2843bff6bc4baa11984e4396060dcfe7043
Stephanie Yoon <stephanieyoon@Stephanies-MacBook-Air.local>
2024-07-22 22:00:08 -0400

    update copy

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,12 +1,15 @@
-import DefaultLayout from "@/layouts/default";
-import Section from "@/layouts/section";
-import Link from "next/link";
-import Image from "next/image";
+import DefaultLayout from '@/layouts/default';
+import Section from '@/layouts/section';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <Section maxWidth="max-w-3xl" isFirst>
+      <Section
+        maxWidth="max-w-3xl"
+        isFirst
+      >
         <div className="text-3xl sm:text-4xl pb-4">About Me</div>
         <div className="md:grid md:grid-cols-5 md:gap-6 pb-5">
           <div className="h-full md:col-span-2 md:flex md:flex-col">
@@ -36,8 +39,8 @@ export default function IndexPage() {
               A few months later, I started taking formal lessons with Maria
               Meirelles, an accomplished Detroit pianist and teacher, who came
               out of retirement to take me as a student. She worked with me to
-              develop not only strong technical foundations and effective
-              practice habits, but also a deep love for music.*
+              develop strong technical foundations and effective practice
+              habits, as well as a deep love for music.*
             </Paragraph>
             <Paragraph isLast>
               My late start has given me a better understanding of how to help
@@ -54,30 +57,30 @@ export default function IndexPage() {
             and I was admitted to both Eastman and Juilliard on scholarship.
           </Heading>
           <Paragraph isLast>
-            Some of these prizes include first place at the{" "}
+            Some of these prizes include first place at the{' '}
             <Link
               className="font-normal text-secondary tap-highlight-transparent data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity"
               target="_blank"
               href="https://sites.uco.edu/cfad/academics/music/the-kuleshov/past-winners"
             >
               Kuleshov
-            </Link>{" "}
-            International Piano Competition, third place in the{" "}
+            </Link>{' '}
+            International Piano Competition, third place in the{' '}
             <Link
               className="font-normal text-secondary tap-highlight-transparent data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity"
               target="_blank"
               href="https://www.capevincentartscouncil.org/pianocompetitioninfo/2022competitionresults.html"
             >
               Thousand Islands
-            </Link>{" "}
-            International Piano Competition, and honorable mention at the{" "}
+            </Link>{' '}
+            International Piano Competition, and honorable mention at the{' '}
             <Link
               className="font-normal text-secondary tap-highlight-transparent data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity"
               target="_blank"
               href="https://www.facebook.com/GarciaPianoCompetition/posts/pfbid0P6knJRPMaJgNt6v6tvB6TA4SWhLNLAvuHaL4WVzhGr4rMcLqJKxKhtMK1FXqmZNzl?rdid=ozmDJA8i1MX4enYb"
             >
               Marian Garcia
-            </Link>{" "}
+            </Link>{' '}
             International Piano Competition.
           </Paragraph>
           <Image
@@ -138,10 +141,10 @@ export default function IndexPage() {
           </div>
         </div>
         <div className="text-sm font-light text-default-500 pt-5">
-          *I would be remiss if I didn't thank all of the teachers who played a
-          pivotal role in my growth not only as a musician and educator, but
-          also as a person: Maria Meirelles, Dizhou Zhao, Nelita True, Boris
-          Slutsky, Alan Chow, and Julian Martin.
+          *I would be remiss if I didn't thank all of the teachers who have
+          played a pivotal role in my life &mdash; not only as a musician and
+          educator, but also as a person &mdash; Maria Meirelles, Dizhou Zhao,
+          Nelita True, Boris Slutsky, Alan Chow, and Julian Martin.
         </div>
       </Section>
     </DefaultLayout>
@@ -160,7 +163,7 @@ function Paragraph({
   isLast?: boolean;
 }) {
   return (
-    <div className={`${isLast ? "" : "pb-4"} font-light text-default-600`}>
+    <div className={`${isLast ? '' : 'pb-4'} font-light text-default-600`}>
       {children}
     </div>
   );
