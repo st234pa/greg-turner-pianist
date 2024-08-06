34fb820b113ecf586c02171d55c8ba34fa9071e2
Stephanie Yoon <stephanieyoon@Stephanies-MacBook-Air.local>
2024-08-06 00:05:19 -0400

    update post api endpts

--- pages/api/post/[slug].ts
+++ pages/api/post/[slug].ts
@@ -1,9 +1,6 @@
 import { createClient } from 'contentful';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/search-parameters/full-text-search-on-a-field/query-entries/console
-// https://stackoverflow.com/questions/70103027/contentful-how-to-get-an-entry-using-nothing-but-one-of-its-fields-or-how-to
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,3 +1,37 @@
-import { createClient } from 'contentful';
+import { createClient, EntryCollection, EntrySkeletonType } from 'contentful';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-// https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/search-parameters/limit
+// https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/search-parameters/skip/query-entries/console
+
+type BlogPostsRequestBody = {
+  limit: number | undefined;
+  skip: number | undefined;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const requestBody = JSON.parse(req.body) as BlogPostsRequestBody;
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID!,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  });
+
+  client
+    .getEntries({
+      content_type: 'postcard',
+      limit: requestBody.limit,
+      skip: requestBody.skip,
+    })
+    .then((entryCollection: EntryCollection<EntrySkeletonType>) => {
+      if (entryCollection.errors && entryCollection.errors.length > 0) {
+        throw new Error();
+      }
+      res.status(200).json(entryCollection);
+    })
+    .catch(() => {
+      res.status(400).end();
+    });
+}
